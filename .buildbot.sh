4036d392d50c6a6061587dc635e90aa2932efd6c
Edd Barrett <vext01@gmail.com>
2018-11-27 16:11:02 +0000

    Revise CI script.

--- .buildbot.sh
+++ .buildbot.sh
@@ -5,10 +5,5 @@
 ./x.py clean  # We don't clone afresh to save time and bandwidth.
 
 # Note that the gdb must be Python enabled.
-#
-# We are running a subset of tests for now since the gdb compile tests are
-# currently failing due to out of data upstream code:
-# https://github.com/softdevteam/ykrustc/pull/14#issuecomment-440608570
-PATH=/opt/gdb-8.2/bin:${PATH} RUST_BACKTRACE=1 \
-    ./x.py test --config .buildbot.toml \
-    src/test/ui src/test/compile-fail src/test/run-pass
+PATH=/opt/gdb-8.2/bin:${PATH} RUST_BACKTRACE=1 YK_DEBUG_SECTIONS=1 \
+    ./x.py test --config .buildbot.toml
